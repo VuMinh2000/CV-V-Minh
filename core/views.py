8fb7b43244989dbf0dfe3ac19861f4326bb2663d
Vũ Minh <1851060095@e.tlu.edu.vn>
2022-08-11 15:04:55 +0700

    add contact email

--- core/views.py
+++ core/views.py
@@ -1,7 +1,22 @@
+from django.contrib import messages
 from django.shortcuts import render
-from django.views import View
+from django.http import HttpResponseRedirect
+from .models import Contact
 
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, 'index.html')
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, subject=subject, message=message)
+        contact.save()
+
+        messages.success(request, "Message sent successfully!!!")
+        return HttpResponseRedirect('/')
+
--- myCV/urls.py
+++ myCV/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', views.index, name="index"),
+    path('contact', views.contact, name="contact")
 ]
